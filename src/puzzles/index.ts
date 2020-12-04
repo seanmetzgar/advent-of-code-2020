c1b76cf7392d08da1d909ca530ddecda78534f82
Sean Metzgar <sean.metzgar@gmail.com>
2020-12-04 11:45:53 -0500

    🎄 Completes Day 3

--- src/puzzles/index.ts
+++ src/puzzles/index.ts
@@ -1,9 +1,11 @@
 import day01_1 from "./day01-1";
 import day01_2 from "./day01-2";
 import day02 from "./day02";
+import day03 from "./day03";
 
 export default {
   day01_1: day01_1,
   day01_2: day01_2,
-  day02: day02
+  day02: day02,
+  day03: day03
 };
